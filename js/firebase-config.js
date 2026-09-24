0dfff9e40fbf09a4a8e1de25791906331bde9a1e
MichCaLo <kimberly.canelo.isemed.fdr@gmail.com>
2026-09-24 09:58:46 -0600

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,16 @@
+/* =========================================================
+   CONFIGURACIÓN DE FIREBASE – EduAsistencia
+   Proyecto: control-academico-628ff
+   ---------------------------------------------------------
+   No agregue líneas "import" ni "initializeApp" a este archivo:
+   el sistema ya inicia Firebase por su cuenta.
+   ========================================================= */
+const firebaseConfig = {
+  apiKey: "AIzaSyCcF9yU9A_iWxhlhEyDB_mFwRdkBX86q8U",
+  authDomain: "control-academico-628ff.firebaseapp.com",
+  projectId: "control-academico-628ff",
+  storageBucket: "control-academico-628ff.firebasestorage.app",
+  messagingSenderId: "698889293741",
+  appId: "1:698889293741:web:4402f87406191b5d159b24",
+  measurementId: "G-SZVSS2XLSN"
+};
